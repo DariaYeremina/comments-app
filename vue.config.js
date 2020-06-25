815d13b57794a2afa06150582e5fca8cb2ca33c8
DariaYeremina <dariaeremina12@gmail.com>
2020-06-25 08:34:42 +0200

    remove gh-pages

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,5 @@ module.exports = {
         prependData: '@import "@/styles/_variables.scss";',
       },
     },
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/comments-app/'
-    : '/',
+  }
 };
